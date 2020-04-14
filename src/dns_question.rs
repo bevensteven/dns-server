b6fe84d25eb63cc163cfa107564b1436cfc2381a
bevensteven <struong@berkeley.edu>
2020-04-14 10:20:19 -0700

    add fields: DnsRecord,DnsQuestion

--- src/dns_question.rs
+++ src/dns_question.rs
@@ -1,7 +1,13 @@
-pub struct DnsQuestion {}
+pub struct DnsQuestion {
+    pub name: String,
+    pub qtype: QueryType,
+}
 
 impl DnsQuestion {
-    pub fn new() -> DnsQuestion {
-        DnsQuestion {}
+    pub fn new(name: String, qtype: QueryType) -> DnsQuestion {
+        DnsQuestion {
+            name: name,
+            qtype: qtype
+        }
     }
 }
--- src/dns_record.rs
+++ src/dns_record.rs
@@ -1,4 +1,18 @@
-pub struct DnsRecord {}
+#[derive(Debug,Clone,PartialEq,Eq,Hash,PartialOrd,Ord)]
+#[allow(dead_code)]
+pub enum DnsRecord {
+    UNKNOWN {
+        domain: String,
+        qtype: u16,
+        data_len: u16,
+        ttl: u32
+    }, // 0
+    A {
+        domain: String,
+        addr: Ipv4Addr,
+        ttl: u32
+    }, // 1
+}
 
 impl DnsRecord {
     pub fn new() -> DnsRecord {
