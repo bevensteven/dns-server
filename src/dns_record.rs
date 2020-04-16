52db11d2cf48df9f2d0cc1da04e2764ac6885fc6
bevensteven <struong@berkeley.edu>
2020-04-15 23:49:30 -0700

    feat: impl DnsRecord

--- src/dns_record.rs
+++ src/dns_record.rs
@@ -15,7 +15,41 @@ pub enum DnsRecord {
 }
 
 impl DnsRecord {
-    pub fn new() -> DnsRecord {
-        DnsRecord {}
+
+    pub fn read(buffer: &mut BytePacketBuffer) -> Result<DnsRecord> {
+        let mut domain = String::new();
+        buffer.read_qname(&mut domain)?;
+
+        let qtype_num = buffer.read_u16()?;
+        let qtype_num = QueryType::from_num(qtype_num);
+        let _ = buffer.read_u16()?;
+        let ttl = buffer.read_u32()?;
+        let data_len = buffer.read_u16()?;
+
+        match qtype {
+            QueryType::A => {
+                let raw_addr = buffer.read_u32()?;
+                let addr = Ipv4Addr::new(((raw_addr >> 24) & 0xFF) as u8,
+                                         ((raw_addr >> 16) & 0xFF) as u8,
+                                         ((raw_addr >> 8) & 0xFF) as u8,
+                                         ((raw_addr >> 0) & 0xFF) as u8);
+                
+                Ok(DnsRecord::A {
+                    domain: domain,
+                    addr: addr,
+                    ttl: ttl
+                })
+            },
+            QueryType::UNKNOWN(_) => {
+                buffer.step(data_len as usize)?;
+
+                Ok(DnsRecord::UNKNOWN {
+                    domain: domain,
+                    qtype: qtype_num,
+                    data_len: data_len,
+                    ttl: ttl
+                })
+            }
+        }
     }
 }
