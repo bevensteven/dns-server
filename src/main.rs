e872685eba3dc300a5b2fcc93efe711af05c7d46
bevensteven <struong@berkeley.edu>
2020-04-16 10:44:04 -0700

    feat: impl main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::fs::File;
+use std::io::Read;
+
+mod dns;
+use dns::byte_packet_buffer::BytePacketBuffer;
+use dns::dns_packet::DnsPacket;
+
 fn main() {
-    println!("Hello, world!");
+    let mut f = File::open("response_packet.txt").unwrap();
+    let mut buffer = BytePacketBuffer::new();
+    f.read(&mut buffer.buf).unwrap();
+
+    let packet = DnsPacket::from_buffer(&mut buffer).unwrap();
+    println!("{:#?}", packet.header);
+
+    for q in packet.questions {
+        println!("{:#?}", q);
+    }
+    for rec in packet.answers {
+        println!("{:#?}", rec);
+    }
+    for rec in packet.authorities {
+        println!("{:#?}", rec);
+    }
+    for rec in packet.resources {
+        println!("{:#?}", rec);
+    }
 }
